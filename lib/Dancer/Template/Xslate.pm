d2cbecd762b0de2a31625f5cfe01a0e26bb68248
Richard Simões <rsimoes@cpan.org>
2011-12-10 14:51:14 +0000

    Import of RSIMOES/Dancer-Template-Xslate-0.010_001-TRIAL from CPAN.
    gitpan-cpan-distribution: Dancer-Template-Xslate
    gitpan-cpan-version:      0.010_001-TRIAL
    gitpan-cpan-path:         RSIMOES/Dancer-Template-Xslate-0.010_001-TRIAL.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     developer

--- lib/Dancer/Template/Xslate.pm
+++ lib/Dancer/Template/Xslate.pm
@@ -1,39 +1,54 @@
 package Dancer::Template::Xslate;
-BEGIN {
-  $Dancer::Template::Xslate::VERSION = '0.01';
-}
 
-# ABSTRACT: Text::Xslate wrapper for Dancer
 
 use strict;
 use warnings;
 
+use Carp;
+use Dancer::App;
+use File::Spec::Functions qw(abs2rel rel2abs);
 use Text::Xslate;
 
 use base 'Dancer::Template::Abstract';
 
-my $_engine;
+our $VERSION = '0.010_001'; # VERSION
+# ABSTRACT: Text::Xslate wrapper for Dancer
+
+# Note: The standard Xslate template extension is
+# "tx" but kept to "tt" for backward compatibility.
 
 sub init {
-    my $self = shift;
+    my ($self) = @_;
+    my $app    = Dancer::App->current;
+    my %xslate_args = %{$self->config};
 
-    my %args = (
-        %{$self->config},
-    );
+    ## set default path for header/footer etc.
+    $xslate_args{path} ||= [];
+    my $views_dir = $app->setting('views');
+    push @{$xslate_args{path}}, $views_dir
+        if not grep { $_ eq $views_dir } @{$xslate_args{path}};
 
-    $_engine = Text::Xslate->new(%args);
+    ## for those who read Text::Xslate instead of Dancer::Template::Abstract
+    $self->config->{extension} = $xslate_args{suffix}
+        if exists $xslate_args{suffix};
+
+    ## avoid 'Text::Xslate: Unknown option(s): extension'
+    $xslate_args{suffix} = delete $xslate_args{extension}
+        if exists $xslate_args{extension};
+
+    $self->{driver} = Text::Xslate->new(%xslate_args);
+    return;
 }
 
 sub render {
     my ($self, $template, $tokens) = @_;
-
-    my $content = eval {
-        $_engine->render($template, $tokens)
-    };
+    my $app    = Dancer::App->current;
+    $template = abs2rel( rel2abs($template), $app->setting('views') );
+    my $xslate = $self->{driver};
+    my $content = $xslate->render($template, $tokens);
 
     if (my $err = $@) {
-        my $error = qq/Couldn't render template "$err"/;
-        die $error;
+        croak qq[Couldn't render template "$err"];
     }
 
     return $content;
@@ -51,7 +66,7 @@ Dancer::Template::Xslate - Text::Xslate wrapper for Dancer
 
 =head1 VERSION
 
-version 0.01
+version 0.010_001
 
 =head1 DESCRIPTION
 
@@ -69,11 +84,11 @@ You can configure L<Text::Xslate> :
 
     template: xslate
     engines:
-        xslate:
-            cache_dir  => "/www/.../xslate_cache",
-            cache      => 1,
-            module =>
-                - Text::Xslate::Bridge::TT2 # to keep partial compatibility
+      xslate:
+        cache_dir  => "/www/../xslate_cache",
+        cache      => 1,
+        module =>
+          - Text::Xslate::Bridge::TT2 # to keep partial compatibility
 
 =head1 SEE ALSO
 
@@ -85,7 +100,7 @@ franck cuny <franck@lumberjaph.net>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by franck cuny.
+This software is copyright (c) 2011 by franck cuny.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
